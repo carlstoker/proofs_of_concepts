9d67c39758b02af8f4f8337ed23a1c0d84184b3b
carlstoker <40079934+carlstoker@users.noreply.github.com>
2019-04-06 00:11:30 -0400

    FizzBuzz cleanup

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,11 +4,4 @@
 #For each multiple of 5, print "Buzz" instead of the number.
 #For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 for i in range(1,101):
-    if i % 15 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
-    else:
-        print(i)
+    print('Fizz'*(i%3==0)+'Buzz'*(i%5==0) or i)
