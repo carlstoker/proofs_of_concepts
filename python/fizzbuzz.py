bf35e9b0fdfe43e3abb590302c1873d7478136ef
carlstoker <40079934+carlstoker@users.noreply.github.com>
2020-01-20 10:24:52 -0500

    Comment, docstring, and spacing cleanup.

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-#Write a short program that prints each number from 1 to 100 on a new line.
-#For each multiple of 3, print "Fizz" instead of the number.
-#For each multiple of 5, print "Buzz" instead of the number.
-#For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+"""FizzBuzz"""
 for i in range(1,101):
     print('Fizz'*(i%3==0)+'Buzz'*(i%5==0) or i)
